53b6e0cceb276ca866c1a842fbff0ba021c47461
mu.dawood <mu.dawood@outlook.com>
2025-05-24 21:28:32 +0300

    ...

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
@@ -12,25 +12,34 @@ import AdvancedUsage from './pages/AdvancedUsage';
 import ApiReference from './pages/ApiReference';
 import './App.css';
 
+function AppContent() {
+  const location = useLocation();
+  const isDocsPage = location.pathname.startsWith('/docs');
+
+  return (
+    <div className="app">
+      {!isDocsPage && <Header />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/docs" element={<DocumentationLayout />}>
+          <Route index element={<DocsOverview />} />
+          <Route path="getting-started" element={<GettingStarted />} />
+          <Route path="examples" element={<RealWorldExamples />} />
+          <Route path="attributes" element={<ValidationAttributes />} />
+          <Route path="advanced" element={<AdvancedUsage />} />
+          <Route path="api" element={<ApiReference />} />
+        </Route>
+      </Routes>
+      {!isDocsPage && <Footer />}
+      <ScrollToTop />
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="app">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/docs" element={<DocumentationLayout />}>
-            <Route index element={<DocsOverview />} />
-            <Route path="getting-started" element={<GettingStarted />} />
-            <Route path="examples" element={<RealWorldExamples />} />
-            <Route path="attributes" element={<ValidationAttributes />} />
-            <Route path="advanced" element={<AdvancedUsage />} />
-            <Route path="api" element={<ApiReference />} />
-          </Route>
-        </Routes>
-        <Footer />
-        <ScrollToTop />
-      </div>
+      <AppContent />
     </Router>
   );
 }
