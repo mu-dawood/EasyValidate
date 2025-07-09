e9b6c32c0228bb7aca066387cfe8c9f432073723
mu.dawood <mu.dawood@outlook.com>
2025-07-09 07:00:45 +0300

    ....

--- EasyValidate.ConsoleTest/Program.cs
+++ EasyValidate.ConsoleTest/Program.cs
@@ -1,4 +1,10 @@
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 
 
-BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, ManualConfig.CreateEmpty()
+    .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+    .WithOptions(ConfigOptions.JoinSummary)
+    .WithOptions(ConfigOptions.KeepBenchmarkFiles)
+    .HideColumns("Job", "Toolchain","IterationCount","MaxWarmupIterationCount","WarmupCount")
+    );
