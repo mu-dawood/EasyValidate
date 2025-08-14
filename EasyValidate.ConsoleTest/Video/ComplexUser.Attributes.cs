18afff84f009711e25abc1c1bdca755186ba10a8
mu.dawood <mu.dawood@outlook.com>
2025-08-14 12:44:44 +0300

    ...

---
+++ EasyValidate.ConsoleTest/Video/ComplexUser.Attributes.cs
@@ -0,0 +1,44 @@
+using EasyValidate.Core.Abstraction;
+using EasyValidate.Core.Attributes;
+
+namespace EasyValidate.ConsoleTest.Video;
+
+public partial class ComplexUserWithAttributes : IValidate
+{
+    [NotEmpty]
+    public string Name { get; set; } = string.Empty;
+
+    [NotNull]
+    public User? UserDetails { get; set; }
+
+    [HasElements]
+    public List<User> Friends { get; set; } = [];
+
+    [Range(0, 120)]
+    public int Age { get; set; }
+
+    [NotEmpty]
+    public string Address { get; set; } = string.Empty;
+
+    [Phone]
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    [PastDate]
+    public DateTime DateOfBirth { get; set; }
+
+    [EqualTo(true)]
+    public bool IsActive { get; set; }
+
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [HasElements]
+    public List<string> Tags { get; set; } = [];
+
+    [Range(0.0, 5.0)]
+    public double Rating { get; set; }
+
+    [NotDefault()]
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+}
